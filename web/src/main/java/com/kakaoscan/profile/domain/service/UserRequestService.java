d192b30a6365d190fdb92d4b2b55307f866abaaa
ekfkawl <ekfkawl>
2023-03-08 06:32:15 +0900

    Feat: 계정별 사용 횟수 동기화
    일일 제한 횟수를 remoteAddress로도 추가 체크하여 계정별 동기화 한다

--- web/src/main/java/com/kakaoscan/profile/domain/service/UserRequestService.java
+++ web/src/main/java/com/kakaoscan/profile/domain/service/UserRequestService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,18 +19,18 @@ public class UserRequestService {
     private final UserRequestRepository userRequestRepository;
 
     @Transactional
-    public long getUseCount(String remoteAddress) {
-        Optional<UserRequest> userRequest = userRequestRepository.findByRemoteAddressAndLastUseDt(remoteAddress, LocalDate.now());
+    public long getUseCount(String email) {
+//        Optional<UserRequest> userRequest = userRequestRepository.findByRemoteAddressAndLastUseDt(email, LocalDate.now());
+        Optional<UserRequest> userRequest = userRequestRepository.findLockById(email);
 
         return userRequest.map(UserRequest::getUseCount).orElse(0L);
     }
 
     @Transactional
-    public boolean updateUseCount(String remoteAddress) {
-
+    public boolean updateUseCount(String email, String remoteAddress) {
         long count = 1;
 
-        Optional<UserRequest> userRequest = userRequestRepository.findLockById(remoteAddress);
+        Optional<UserRequest> userRequest = userRequestRepository.findLockById(email);
         if (userRequest.isPresent()) {
 
             if (LocalDate.now().equals(userRequest.get().getLastUseDt())) {
@@ -35,11 +38,35 @@ public class UserRequestService {
             }
         }
         userRequestRepository.save(UserRequest.builder()
+                .email(email)
                 .remoteAddress(remoteAddress)
                 .useCount(count)
                 .build());
 
         return true;
+    }
 
+    @Transactional
+    public boolean syncUserUseCount(String remoteAddress, LocalDate localDate) {
+
+        List<UserRequest> userRequests = userRequestRepository.findByRemoteAddressAndLastUseDt(remoteAddress, localDate);
+
+        if (userRequests.size() > 1) {
+            Collections.sort(userRequests, new Comparator<UserRequest>() {
+                @Override
+                public int compare(UserRequest o1, UserRequest o2) {
+                    return (int) (o2.getUseCount() - o1.getUseCount());
+                }
+            });
+
+            long useCount = userRequests.get(0).getUseCount();
+            for (UserRequest userRequest : userRequests) {
+                userRequest.setUseCount(useCount);
+            }
+
+            return true;
+        }
+
+        return false;
     }
 }
