4aeb551705f18c4c4f1083904ec58838b6150e63
ekfkawl <ekfkawl>
2023-03-08 06:17:41 +0900

    Feat: WebSocketSession에 user 전달

--- web/src/main/java/com/kakaoscan/profile/domain/interceptor/HandshakeInterceptor.java
+++ web/src/main/java/com/kakaoscan/profile/domain/interceptor/HandshakeInterceptor.java
@@ -1,5 +1,6 @@
 package com.kakaoscan.profile.domain.interceptor;
 
+import com.kakaoscan.profile.global.oauth.OAuthAttributes;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
@@ -8,6 +9,7 @@ import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Map;
 
 import static com.kakaoscan.profile.utils.GenerateUtils.StrToMD5;
@@ -28,6 +30,13 @@ public class HandshakeInterceptor extends HttpSessionHandshakeInterceptor {
 
         String remoteAddress = StrToMD5(getRemoteAddress(httpRequest), "");
         attributes.put("remoteAddress", remoteAddress);
+
+        HttpSession httpSession = httpRequest.getSession();
+        Object userObj = httpSession.getAttribute("user");
+        if (userObj instanceof OAuthAttributes) {
+            OAuthAttributes user = (OAuthAttributes) userObj;
+            attributes.put("user", user);
+        }
 
         return super.beforeHandshake(request, response, wsHandler, attributes);
     }
