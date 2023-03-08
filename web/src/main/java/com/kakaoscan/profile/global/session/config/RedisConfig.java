d9ff1967eefbc7822d568fef048cde377fb0c796
ekfkawl <ekfkawl>
2023-03-08 23:30:38 +0900

    Feat: redis spring 연동

---
+++ web/src/main/java/com/kakaoscan/profile/global/session/config/RedisConfig.java
@@ -0,0 +1,23 @@
+package com.kakaoscan.profile.global.session.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+@Profile("prod")
+public class RedisConfig {
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<?, ?> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        return template;
+    }
+}
