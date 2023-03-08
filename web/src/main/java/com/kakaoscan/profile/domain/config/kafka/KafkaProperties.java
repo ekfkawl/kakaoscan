6423a6932f34315362eb0ba395d7e76ac838b8e3
ekfkawl <ekfkawl>
2023-03-08 23:27:04 +0900

    Chore: @Component로 bean 등록

--- web/src/main/java/com/kakaoscan/profile/domain/config/kafka/KafkaProperties.java
+++ web/src/main/java/com/kakaoscan/profile/domain/config/kafka/KafkaProperties.java
@@ -2,10 +2,10 @@ package com.kakaoscan.profile.domain.config.kafka;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
-@ConfigurationProperties("kafka")
 @Getter
+@Component
 public class KafkaProperties {
     public static final String TOPIC = "kakaoscan.mail.send";
     public static final String CONSUMER_GROUP_ID = "group.kakaoscan.mail.send";
