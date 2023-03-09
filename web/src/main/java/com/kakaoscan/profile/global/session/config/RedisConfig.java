2395e4286b302fa5085502e561bd9f33704cad27
ekfkawl <ekfkawl>
2023-03-10 05:06:54 +0900

    Chore: redis config

--- web/src/main/java/com/kakaoscan/profile/global/session/config/RedisConfig.java
+++ web/src/main/java/com/kakaoscan/profile/global/session/config/RedisConfig.java
@@ -1,17 +1,32 @@
 package com.kakaoscan.profile.global.session.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
-@Configuration
+import java.time.Duration;
+
 @Profile("prod")
+@Configuration
+@EnableCaching
 public class RedisConfig {
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(redisHost, redisPort);
     }
 
     @Bean
@@ -19,5 +34,14 @@ public class RedisConfig {
         RedisTemplate<?, ?> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory());
         return template;
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofHours(1)); // n시간
+        return RedisCacheManager.builder(redisConnectionFactory())
+                .cacheDefaults(cacheConfiguration)
+                .build();
     }
 }
