2d3074930d65253d4d99531d347c94bafb5bc675
ekfkawl <ekfkawl>
2023-03-11 07:20:59 +0900

    Test: check user entity mapping

--- web/src/test/java/com/kakaoscan/profile/domain/repository/UserRepositoryTest.java
+++ web/src/test/java/com/kakaoscan/profile/domain/repository/UserRepositoryTest.java
@@ -2,9 +2,12 @@ package com.kakaoscan.profile.domain.repository;
 
 import com.kakaoscan.profile.domain.config.TestProperties;
 import com.kakaoscan.profile.domain.entity.User;
+import com.kakaoscan.profile.domain.entity.UserHistory;
 import com.kakaoscan.profile.domain.entity.UserRequestUnlock;
 import com.kakaoscan.profile.domain.respon.enums.Role;
+import com.kakaoscan.profile.utils.GenerateUtils;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,20 +20,24 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 @Import(TestProperties.class)
 @SpringBootTest
 class UserRepositoryTest {
+    @Autowired
+    UserRepository userRepository;
 
     @Autowired
-    private UserRepository userRepository;
+    UserRequestUnlockRepository userRequestUnlockRepository;
 
     @Autowired
-    private UserRequestUnlockRepository userRequestUnlockRepository;
+    UserHistoryRepository userHistoryRepository;
 
     @Test
+    @DisplayName("유저 entity 관계 맵핑 확인")
     void testUserMapping() {
         // given
         User user = User.builder()
                 .email("test@test.com")
                 .role(Role.USER)
                 .modifyDt(LocalDateTime.now())
+                .createDt(LocalDateTime.now())
                 .build();
         userRepository.save(user);
 
@@ -41,12 +48,24 @@ class UserRepositoryTest {
                 .build();
         userRequestUnlockRepository.save(requestUnlock);
 
+        for (int i = 1; i <= 2; i++) {
+            UserHistory userHistory = UserHistory.builder()
+                    .email("test@test.com")
+                    .phoneNumberMd5(GenerateUtils.StrToMD5("01012345678", ""))
+                    .urls(String.format("test%d.com", i))
+                    .createDt(LocalDateTime.now())
+                    .build();
+            userHistoryRepository.save(userHistory);
+        }
+
         // when
-        User user1 = userRepository.findById("test@test.com").orElse(null);
+        User user1 = userRepository.findByEmail("test@test.com").orElse(null);
         UserRequestUnlock userRequestUnlock = userRequestUnlockRepository.findById("test@test.com").orElse(null);
 
         // then
         assertThat(user1.getRequestUnlock().getEmail()).isEqualTo(userRequestUnlock.getUser().getEmail());
+        assertThat(user1.getHistoryList().size()).isEqualTo(2);
+
     }
 
     @AfterEach
