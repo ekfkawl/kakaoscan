13382dcad69a5b2e4ac018cbe377deaf64c94c56
ekfkawl <ekfkawl>
2023-03-13 05:14:14 +0900

    Chore: 로그인 성공 시, 사용 횟수 동기화

--- web/src/main/java/com/kakaoscan/profile/global/security/handler/CustomAuthenticationSuccessHandler.java
+++ web/src/main/java/com/kakaoscan/profile/global/security/handler/CustomAuthenticationSuccessHandler.java
@@ -1,5 +1,8 @@
 package com.kakaoscan.profile.global.security.handler;
 
+import com.kakaoscan.profile.domain.service.UserRequestService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
@@ -9,16 +12,30 @@ import org.springframework.stereotype.Component;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
 
+import static com.kakaoscan.profile.utils.GenerateUtils.StrToMD5;
+import static com.kakaoscan.profile.utils.HttpRequestUtils.getRemoteAddress;
+
+@Log4j2
+@RequiredArgsConstructor
 @Component
 public class CustomAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
     private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
 
+    private final UserRequestService userRequestService;
+
     @Override
     public void onAuthenticationSuccess(final HttpServletRequest request, final HttpServletResponse response, final Authentication authentication) throws IOException {
+        // 계정 별 사용 횟수 동기화
+        String remoteAddress = StrToMD5(getRemoteAddress(request), "");
+        userRequestService.syncUserUseCount(remoteAddress, LocalDate.now());
+        log.info("login client remote address : {}, {}", getRemoteAddress(request), remoteAddress);
+
         setDefaultTargetUrl("/");
 
         redirectStrategy.sendRedirect(request, response, getDefaultTargetUrl());
     }
 }
+
