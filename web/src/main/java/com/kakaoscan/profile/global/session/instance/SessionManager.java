efbb2c828c0b1a3ab231f008b9d6f2faffbd77d0
ekfkawl <ekfkawl>
2023-03-14 13:33:04 +0900

    Chore: session format

--- web/src/main/java/com/kakaoscan/profile/global/session/instance/SessionManager.java
+++ web/src/main/java/com/kakaoscan/profile/global/session/instance/SessionManager.java
@@ -1,7 +1,7 @@
 package com.kakaoscan.profile.global.session.instance;
 
 public interface SessionManager {
-    public static final String SESSION_FORMAT = "user:%s";
+    public static final String SESSION_FORMAT = "user::%s";
     public static final String SESSION_KEY = "email-hash";
     void setValue(String key, Object value);
     Object getValue(String key);
