61fd26277c4ea0f102883aa279285d082565ec7a
ekfkawl <ekfkawl>
2023-03-15 02:48:52 +0900

    Chore: user entity findAll() n+1

--- web/src/main/java/com/kakaoscan/profile/domain/repository/UserRepository.java
+++ web/src/main/java/com/kakaoscan/profile/domain/repository/UserRepository.java
@@ -1,14 +1,19 @@
 package com.kakaoscan.profile.domain.repository;
 
 import com.kakaoscan.profile.domain.entity.User;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
     @EntityGraph(attributePaths = "historyList")
     Optional<User> findByEmail(String email);
+
+    @EntityGraph(attributePaths = {"requestUnlock", "request", "historyList"})
+    List<User> findAll(Sort sort);
 }
