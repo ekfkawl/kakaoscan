95375cc0ef3119e066e3c395b0e5f3451ff10edc
ekfkawl <ekfkawl>
2023-03-21 02:19:29 +0900

    Chore: object mapper static 선언

--- web/src/main/java/com/kakaoscan/profile/domain/kafka/mapper/KafkaMessageDeserializer.java
+++ web/src/main/java/com/kakaoscan/profile/domain/kafka/mapper/KafkaMessageDeserializer.java
@@ -7,8 +7,9 @@ import java.io.IOException;
 import java.util.Map;
 
 public class KafkaMessageDeserializer {
+    private static final ObjectMapper mapper = new ObjectMapper();
+
     public static <T extends KafkaEvent> T deserialize(Map<String, Object> map, Class<T> clazz) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
         return mapper.convertValue(map, clazz);
     }
 }
