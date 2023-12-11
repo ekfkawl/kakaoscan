62a83b57776079e828a529ccd3776aeb211e9dcb
ekfkawl <ekfkawl>
2023-12-12 06:21:53 +0900

    feat: user register custom hooks
    - apply hook for RegisterForm
    - regex

---
+++ client/src/hooks/useEmailValidation.ts
@@ -0,0 +1,30 @@
+import {useState} from 'react';
+import {isEmail} from "../utils/validateAuth";
+
+const useEmailValidation = () => {
+    const [email, setEmail] = useState<string>('');
+    const [error, setError] = useState<string>('');
+
+    const validateEmail = (email: string): string => {
+        if (!isEmail(email)) {
+            return "올바른 이메일 형식이 아닙니다.";
+        }
+
+        const allowedDomains = ["google.com", "naver.com"];
+        const domain = email.split('@')[1];
+        if (!allowedDomains.includes(domain)) {
+            return "허용된 이메일 도메인이 아닙니다. (naver, daum만 가능)";
+        }
+
+        return '';
+    };
+
+    const handleBlur = (): void => {
+        const validationResult = validateEmail(email);
+        setError(validationResult);
+    };
+
+    return { email, setEmail, error, handleBlur };
+};
+
+export default useEmailValidation;
---
+++ client/src/hooks/useModalState.ts
@@ -0,0 +1,12 @@
+import {useState} from 'react';
+
+const useModalState = (initialState: boolean = false) => {
+    const [isOpen, setIsOpen] = useState<boolean>(initialState);
+
+    const openModal = () => setIsOpen(true);
+    const closeModal = () => setIsOpen(false);
+
+    return { isOpen, openModal, closeModal };
+};
+
+export default useModalState;
