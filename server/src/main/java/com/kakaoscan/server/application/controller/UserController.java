0f2d43ed81a39e5070296df752294d4fcc786cf4
ekfkawl <ekfkawl>
2023-12-12 22:04:14 +0900

    feat: apply rate limiting to the register api
    - use bucket4j

--- server/src/main/java/com/kakaoscan/server/application/controller/UserController.java
+++ server/src/main/java/com/kakaoscan/server/application/controller/UserController.java
@@ -3,10 +3,15 @@ package com.kakaoscan.server.application.controller;
 import com.kakaoscan.server.application.dto.ApiResponse;
 import com.kakaoscan.server.application.dto.RegisterRequest;
 import com.kakaoscan.server.application.port.UserPort;
+import com.kakaoscan.server.infrastructure.service.RateLimitService;
+import com.kakaoscan.server.infrastructure.utils.WebUtils;
+import io.github.bucket4j.Bucket;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,13 +20,19 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "User", description = "User API")
 public class UserController extends ApiPathPrefix {
     private final UserPort userPort;
+    private final RateLimitService rateLimitService;
 
     @Value("${verify.replace}")
     private String verifyReplace;
 
     @PostMapping("/register")
-    public ResponseEntity<ApiResponse> register(@RequestBody @Valid RegisterRequest registerRequest) {
-        return ResponseEntity.ok(userPort.register(registerRequest));
+    public ResponseEntity<?> register(@RequestBody @Valid RegisterRequest registerRequest, HttpServletRequest request) {
+        Bucket bucket = rateLimitService.resolveBucket(WebUtils.getRemoteAddress(request));
+        if (bucket.tryConsume(1)) {
+            return ResponseEntity.ok(userPort.register(registerRequest));
+        }else {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).body("too many requests");
+        }
     }
 
     @GetMapping("/verify/{token}")
---
+++ server/src/main/java/com/kakaoscan/server/infrastructure/service/RateLimitService.java
@@ -0,0 +1,38 @@
+package com.kakaoscan.server.infrastructure.service;
+
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+public class RateLimitService {
+    private final Object lock = new Object();
+    private final Map<String, Bucket> buckets = new ConcurrentHashMap<>();
+
+    public Bucket createBucket() {
+        long capacity = 3;
+        Refill refill = Refill.greedy(capacity, Duration.ofHours(1));
+        Bandwidth limit = Bandwidth.classic(capacity, refill);
+
+        return Bucket.builder()
+                .addLimit(limit)
+                .build();
+    }
+
+    public Bucket resolveBucket(String remoteAddress) {
+        return buckets.computeIfAbsent(remoteAddress, v -> createBucket());
+    }
+
+    @Scheduled(cron = "0 0 0 * * *") // 매일 자정에 실행
+    public void resetBuckets() {
+        synchronized (lock) {
+            buckets.clear();
+        }
+    }
+}
