448a507ba0ee50d9665fde7140604e484f1605a0
ekfkawl <ekfkawl>
2023-12-13 18:15:55 +0900

    feat: login hook and redux store and slices for jwt token
    - protected routes for authenticated user

---
+++ client/src/components/auth/ProtectedRoute.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
+import { Spinner } from 'flowbite-react';
+
+interface ProtectedRouteProps {
+    children: React.ReactNode;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+    const { isAuthenticated, isInitialized } = useAuth();
+
+    if (!isInitialized) {
+        return (
+            <div className="flex justify-center items-center h-screen dark:bg-gray-900">
+                <Spinner color="success" />
+            </div>
+        );
+    }
+
+    if (!isAuthenticated) {
+        return <Navigate to="/login" />;
+    }
+
+    return <>{children}</>;
+};
+
+export default ProtectedRoute;
---
+++ client/src/hooks/useLogin.ts
@@ -0,0 +1,44 @@
+import { AxiosResponse } from 'axios';
+import { useState } from 'react';
+import axiosInstance from '../utils/axiosInstance';
+import store from '../redux/store';
+import { setToken } from '../redux/slices/authSlice';
+import { ApiResponse } from '../types/apiResponse';
+
+interface LoginRequest {
+    email: string;
+    password: string;
+}
+
+const useLogin = () => {
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string>('');
+
+    const login = async (loginData: LoginRequest): Promise<ApiResponse | null> => {
+        setIsLoading(true);
+        setError('');
+
+        try {
+            const res: AxiosResponse<ApiResponse> = await axiosInstance.post(
+                'api/login',
+                loginData,
+            );
+
+            setIsLoading(false);
+            setError(res.data.message || '');
+
+            store.dispatch(setToken(res.data.data?.accessToken));
+
+            return res.data;
+        } catch (error) {
+            setIsLoading(false);
+            setError('로그인 중 오류가 발생했습니다.');
+
+            return null;
+        }
+    };
+
+    return { login, isLoading, error };
+};
+
+export default useLogin;
