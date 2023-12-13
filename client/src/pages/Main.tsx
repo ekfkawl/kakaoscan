8ca342d27d133e39bb6becb8e9f5939567ddd88f
ekfkawl <ekfkawl>
2023-12-14 01:49:06 +0900

    feat: header with login user

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -4,7 +4,6 @@ const AuthPage: React.FC<PropsWithChildren<{}>> = () => {
     return (
         <section className="bg-white dark:bg-gray-900">
             <div className="grid grid-cols-1 min-h-screen">
-                ddd
             </div>
         </section>
     );
