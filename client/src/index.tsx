5f61f00591f45719bf717cf9c77dec3d2f3bf1b8
ekfkawl <ekfkawl>
2023-12-13 18:17:28 +0900

    refactor: use createRoot instead of root.render()

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,43 +1,17 @@
 import './index.css';
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import AuthPage from './pages/AuthPage';
-import LoginForm from './components/auth/LoginForm';
-import RegisterForm from './components/auth/RegisterForm';
-import Layout from './components/Layout';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import { createRoot } from 'react-dom/client';
+import App from './App';
 
 const container = document.getElementById('root');
 if (container) {
     const root = createRoot(container);
-
     root.render(
         <React.StrictMode>
             <Provider store={store}>
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<Layout />}>
-                            <Route
-                                index
-                                element={
-                                    <AuthPage>
-                                        <LoginForm />
-                                    </AuthPage>
-                                }
-                            />
-                            <Route
-                                path="register"
-                                element={
-                                    <AuthPage>
-                                        <RegisterForm />
-                                    </AuthPage>
-                                }
-                            />
-                        </Route>
-                    </Routes>
-                </BrowserRouter>
+                <App />
             </Provider>
         </React.StrictMode>,
     );
