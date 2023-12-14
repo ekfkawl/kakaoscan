14579f705314c44413a7b7fd3b8fc76c780bc966
ekfkawl <ekfkawl>
2023-12-15 00:12:00 +0900

    feat: main searchbar hook and style

---
+++ client/src/hooks/usePhoneNumberFormat.ts
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+
+const usePhoneNumberFormat = (): [string, (event: React.ChangeEvent<HTMLInputElement>) => void] => {
+    const [phoneNumber, setPhoneNumber] = useState<string>('');
+
+    const formatPhoneNumber = (inputValue: string): string => {
+        if (!inputValue) return inputValue;
+
+        const numbersAndHyphens = inputValue.replace(/[^\d-]/g, '');
+        const numbers = numbersAndHyphens.replace(/-/g, '');
+
+        if (numbers.length < 4) return numbers;
+        if (numbers.length < 7) return `${numbers.slice(0, 3)}-${numbers.slice(3)}`;
+
+        return `${numbers.slice(0, 3)}-${numbers.slice(3, 7)}-${numbers.slice(7, 11)}`;
+    };
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        let formattedPhoneNumber = formatPhoneNumber(event.target.value);
+
+        if (formattedPhoneNumber.endsWith('-')) {
+            formattedPhoneNumber = formattedPhoneNumber.slice(0, -1);
+        }
+
+        setPhoneNumber(formattedPhoneNumber);
+    };
+
+    return [phoneNumber, handleChange];
+};
+
+export default usePhoneNumberFormat;
