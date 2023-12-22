a41ccf89f1a41810613f37eaf2f63f109a78daea
ekfkawl <ekfkawl>
2023-12-23 00:03:24 +0900

    feat: win32 object and logging util

---
+++ win32/common/GuardObjectUtil.pas
@@ -0,0 +1,37 @@
+unit GuardObjectUtil;
+
+interface
+
+function Guard(out Reference; Instance: TObject): IUnknown;
+
+implementation
+
+type
+  TGuardObject = class(TInterfacedObject)
+private
+  FInstance: TObject;
+public
+  constructor Create(Instance: TObject);
+  destructor Destroy; override;
+end;
+
+{ TGuardObject }
+
+constructor TGuardObject.Create(Instance: TObject);
+begin
+  FInstance:= Instance;
+end;
+
+destructor TGuardObject.Destroy;
+begin
+  FInstance.Free;
+  inherited;
+end;
+
+function Guard(out Reference; Instance: TObject): IUnknown;
+begin
+  Result:= TGuardObject.Create(Instance);
+  TObject(Reference):= Instance;
+end;
+
+end.
