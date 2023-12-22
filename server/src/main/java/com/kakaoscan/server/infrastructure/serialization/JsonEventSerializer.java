c63105862fc6f4efd0abfcabf8e71f2d3125b086
ekfkawl <ekfkawl>
2023-12-22 23:34:13 +0900

    feat: json serialization utils

---
+++ server/src/main/java/com/kakaoscan/server/infrastructure/serialization/JsonEventSerializer.java
@@ -0,0 +1,22 @@
+package com.kakaoscan.server.infrastructure.serialization;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.kakaoscan.server.common.utils.ObjectMapperSingleton;
+import com.kakaoscan.server.domain.events.EventMetadata;
+
+public class JsonEventSerializer {
+    private static final ObjectMapper objectMapper = ObjectMapperSingleton.getInstance();
+
+    public static String serialize(EventMetadata event) {
+        try {
+            ObjectNode rootNode = objectMapper.createObjectNode();
+            rootNode.put("eventType", event.getClass().getSimpleName());
+            rootNode.set("data", objectMapper.valueToTree(event));
+
+            return rootNode.toString();
+        } catch (Exception e) {
+            throw new RuntimeException("serialization error", e);
+        }
+    }
+}
