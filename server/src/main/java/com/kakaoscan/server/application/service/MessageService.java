174b22bd1f1733d8248424c382cd60484599216d
ekfkawl <ekfkawl>
2023-12-30 18:36:05 +0900

    fix: identify authenticated users in websocket (#3)

--- server/src/main/java/com/kakaoscan/server/application/service/MessageService.java
+++ server/src/main/java/com/kakaoscan/server/application/service/MessageService.java
@@ -1,11 +1,8 @@
 package com.kakaoscan.server.application.service;
 
 import com.kakaoscan.server.domain.search.model.Message;
-import com.kakaoscan.server.infrastructure.security.JwtTokenProvider;
-import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.SignatureException;
+import com.kakaoscan.server.infrastructure.exception.UserNotVerifiedException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import java.security.Principal;
@@ -13,24 +10,12 @@ import java.security.Principal;
 @Service
 @RequiredArgsConstructor
 public class MessageService {
-    private final JwtTokenProvider jwtTokenProvider;
 
     public Message createMessage(Principal principal, Message.OriginMessage originMessage) {
         if (principal != null) {
             return new Message(principal.getName(), originMessage.getContent());
+        }else {
+            throw new UserNotVerifiedException("websocket principal is empty");
         }
-
-        if (originMessage.getToken() == null || !jwtTokenProvider.validateAccessToken(originMessage.getToken())) {
-            throw new JwtException("invalid or missing token");
-        }
-
-        Authentication authentication;
-        try {
-            authentication = jwtTokenProvider.getAuthentication(originMessage.getToken());
-        } catch (SignatureException e) {
-            throw new JwtException("authentication failed");
-        }
-
-        return new Message(authentication.getName(), originMessage.getContent());
     }
 }
