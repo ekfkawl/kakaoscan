8eb73a66ca1e61246146bc435f87cb9d6a088d49
ekfkawl <ekfkawl>
2023-12-30 18:58:26 +0900

    chore: response message to be sent to the user

---
+++ server/src/main/java/com/kakaoscan/server/infrastructure/constants/ResponseMessages.java
@@ -0,0 +1,8 @@
+package com.kakaoscan.server.infrastructure.constants;
+
+public class ResponseMessages {
+    public static final String SEARCH_QUEUE_WAITING = "대기 인원: %d명";
+    public static final String SEARCH_WAITING = "서버에 연결 중 입니다..";
+    public static final String SEARCH_STARTING = "프로필 조회를 시작합니다..";
+    public static final String SEARCH_ERROR_PING_PONG = "서버 연결에 실패하였습니다.";
+}
