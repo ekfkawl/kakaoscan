c6294d81b67a2d1bf99acc9356fb3ea39da4b7d9
ekfkawl <ekfkawl>
2023-12-30 18:48:00 +0900

    chore: overload logging

--- win32/common/LogUtil.pas
+++ win32/common/LogUtil.pas
@@ -5,7 +5,8 @@ interface
 uses
   System.SysUtils, System.IOUtils;
 
-procedure Log(const Msg: string; E: Exception);
+procedure Log(const Msg: string; E: Exception); overload;
+procedure Log(const Msg: string); overload;
 
 implementation
 
@@ -14,7 +15,7 @@ const LOG_PATH = 'C:\kakaoscan.log';
 procedure Log(const Msg: string; E: Exception);
 var
   LogFile: TextFile;
-  FileName: string;
+  FileName, Log: string;
 begin
   FileName:= LOG_PATH;
   AssignFile(LogFile, FileName);
@@ -24,7 +25,30 @@ begin
     else
       Append(LogFile);
 
-    WriteLn(LogFile, Format('[%s] %s: %s %s: %s', [FormatDateTime('hh:mm:ss', Now), E.ClassName, #13#10, Msg, E.Message]));
+    Log:= Format('[%s] %s: %s %s: %s', [FormatDateTime('hh:mm:ss', Now), E.ClassName, #13#10, Msg, E.Message]);
+    WriteLn(LogFile, Log);
+    WriteLn(Log);
+  finally
+    CloseFile(LogFile);
+  end;
+end;
+
+procedure Log(const Msg: string);
+var
+  LogFile: TextFile;
+  FileName, Log: string;
+begin
+  FileName:= LOG_PATH;
+  AssignFile(LogFile, FileName);
+  try
+    if not FileExists(FileName) then
+      Rewrite(LogFile)
+    else
+      Append(LogFile);
+
+    Log:= Format('[%s] %s', [FormatDateTime('hh:mm:ss', Now), Msg]);
+    WriteLn(LogFile, Log);
+    WriteLn(Log);
   finally
     CloseFile(LogFile);
   end;
