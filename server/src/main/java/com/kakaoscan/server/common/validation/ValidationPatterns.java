a0e78e732d41cc4f0819faa0723aa1f9e45d28e4
ekfkawl <ekfkawl>
2024-01-05 14:09:43 +0900

    chore: check message phone number validity

--- server/src/main/java/com/kakaoscan/server/common/validation/ValidationPatterns.java
+++ server/src/main/java/com/kakaoscan/server/common/validation/ValidationPatterns.java
@@ -3,4 +3,5 @@ package com.kakaoscan.server.common.validation;
 public class ValidationPatterns {
     public static final String EMAIL_AND_DOMAIN = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@(naver\\.com|daum\\.net)$";
     public static final String PASSWORD = "^[A-Za-z\\d~!@#$%^&*()_+]{8,16}$";
+    public static final String ONLY_NUMBER = "\\d+";
 }
