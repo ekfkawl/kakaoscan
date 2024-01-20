a82703974181022e425acfc2c13b6cf42b764e73
ekfkawl <ekfkawl>
2024-01-20 13:29:37 +0900

    feat: rate limit too many request invalid phone number (#12)

--- server/src/main/java/com/kakaoscan/server/application/events/handlers/SearchEventHandler.java
+++ server/src/main/java/com/kakaoscan/server/application/events/handlers/SearchEventHandler.java
@@ -7,21 +7,26 @@ import com.kakaoscan.server.domain.events.model.SearchEvent;
 import com.kakaoscan.server.domain.search.model.Message;
 import com.kakaoscan.server.domain.search.queue.QueueAggregate;
 import com.kakaoscan.server.infrastructure.events.processor.AbstractEventProcessor;
+import com.kakaoscan.server.infrastructure.service.RateLimitService;
+import io.github.bucket4j.Bucket;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
 import java.util.Optional;
 
 import static com.kakaoscan.server.domain.events.enums.EventStatusEnum.*;
-import static com.kakaoscan.server.infrastructure.constants.ResponseMessages.SEARCH_STARTING;
-import static com.kakaoscan.server.infrastructure.constants.ResponseMessages.SEARCH_WAITING;
+import static com.kakaoscan.server.infrastructure.constants.ResponseMessages.*;
 
 @Component
 @RequiredArgsConstructor
+@Log4j2
 public class SearchEventHandler extends AbstractEventProcessor<SearchEvent> {
     private final EventStatusPort eventStatusPort;
     private final QueueAggregate queue;
     private final StompMessageDispatcher messageDispatcher;
+    private final RateLimitService rateLimitService;
 
     @Override
     protected void handleEvent(SearchEvent event) {
@@ -35,6 +40,12 @@ public class SearchEventHandler extends AbstractEventProcessor<SearchEvent> {
             case WAITING -> SEARCH_WAITING;
             case PROCESSING -> (status.getMessage() == null || status.getMessage().isEmpty()) ? SEARCH_STARTING : status.getMessage();
             case SUCCESS, FAILURE -> {
+                if (SEARCH_INVALID_PHONE_NUMBER.equals(status.getMessage())) {
+                    Bucket bucket = rateLimitService.resolveBucket(event.getEmail(), 5, Duration.ofHours(2));
+                    if (!bucket.tryConsume(1)) {
+                        log.info("too many invalid phone number search: {}", event.getEmail());
+                    }
+                }
                 queue.remove(event.getEventId());
                 yield status.getMessage();
             }
