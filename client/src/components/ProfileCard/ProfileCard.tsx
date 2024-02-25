7de4e59a26e94995e19da56100e80b98c3e9728a
ekfkawl <ekfkawl>
2024-02-26 01:55:48 +0900

    chore: profile card image mouse cursor hover css

--- client/src/components/ProfileCard/ProfileCard.tsx
+++ client/src/components/ProfileCard/ProfileCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './ProfileCard.css';
 
 interface ProfileCardProps {
     profileImageUrl: string;
@@ -11,17 +12,17 @@ const ProfileCard: React.FC<ProfileCardProps> = ({ profileImageUrl, name, url, s
     return (
         <div className="flex flex-col items-center pb-8 sm:flex-row">
             <img
-                className="mx-auto mb-4 w-24 h-24 rounded-full sm:ml-0 sm:mr-6"
+                className="mx-auto mb-4 w-24 h-24 rounded-full sm:ml-0 sm:mr-6 profile-img"
                 src={profileImageUrl || '/default-profile.jpg'}
                 alt="avatar"
             />
             <div className="text-center sm:text-left">
                 <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h3>
-                <div className="text-gray-500 dark:text-gray-400">
-                    <a href={url} target="_blank" rel="noopener noreferrer">
-                        {url}
-                    </a>
-                </div>
+                {/*<div className="text-gray-500 dark:text-gray-400">*/}
+                {/*    <a href={url} target="_blank" rel="noopener noreferrer">*/}
+                {/*        {url}*/}
+                {/*    </a>*/}
+                {/*</div>*/}
                 <p className="mt-3 mb-4 max-w-sm text-gray-500 dark:text-gray-400">{statusMessage}</p>
             </div>
         </div>
