5ae588f0086517f83ae751f2bce393e4beeb273a
ekfkawl <ekfkawl>
2024-03-08 02:46:19 +0900

    chore: extract formatPhoneNumber() to utils dir

--- client/src/hooks/formats/usePhoneNumberFormat.ts
+++ client/src/hooks/formats/usePhoneNumberFormat.ts
@@ -1,22 +1,15 @@
 import { useState } from 'react';
+import { formatPhoneNumber } from '../../utils/format/format';
 
-const usePhoneNumberFormat = (): [string, (value: string) => void, (event: React.ChangeEvent<HTMLInputElement>) => void] => {
+const usePhoneNumberFormat = (): [
+    string,
+    (value: string) => void,
+    (event: React.ChangeEvent<HTMLInputElement>) => void,
+] => {
     const [phoneNumber, setPhoneNumber] = useState<string>('');
     const setFormattedPhoneNumber = (value: string) => {
         const formatted = formatPhoneNumber(value);
         setPhoneNumber(formatted);
-    };
-
-    const formatPhoneNumber = (inputValue: string): string => {
-        if (!inputValue) return inputValue;
-
-        const numbersAndHyphens = inputValue.replace(/[^\d-]/g, '');
-        const numbers = numbersAndHyphens.replace(/-/g, '');
-
-        if (numbers.length < 4) return numbers;
-        if (numbers.length < 7) return `${numbers.slice(0, 3)}-${numbers.slice(3)}`;
-
-        return `${numbers.slice(0, 3)}-${numbers.slice(3, 7)}-${numbers.slice(7, 11)}`;
     };
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
