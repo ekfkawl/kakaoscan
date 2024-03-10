6c22b9b995c38d416a1c2ca9acc5809c1651acc8
ekfkawl <ekfkawl>
2024-03-11 01:09:42 +0900

    chore: remove unnecessary dependencies class

--- server/src/main/java/com/kakaoscan/server/domain/user/repository/EmailTokenRepository.java
+++ server/src/main/java/com/kakaoscan/server/domain/user/repository/EmailTokenRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface EmailTokenRepository extends JpaRepository<EmailVerificationToken, Long>, EmailTokenRepositoryCustom {
+public interface EmailTokenRepository extends JpaRepository<EmailVerificationToken, Long> {
     Optional<EmailVerificationToken> findByToken(String token);
 }
