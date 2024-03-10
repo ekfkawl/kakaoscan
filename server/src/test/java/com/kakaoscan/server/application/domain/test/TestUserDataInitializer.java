dc9edd213177156f60ff6bc345d834154087b6f2
ekfkawl <ekfkawl>
2024-03-11 01:08:46 +0900

    refactor: init setup of user data in tests

---
+++ server/src/test/java/com/kakaoscan/server/application/domain/test/TestUserDataInitializer.java
@@ -0,0 +1,34 @@
+package com.kakaoscan.server.application.domain.test;
+
+import com.kakaoscan.server.domain.user.entity.User;
+import com.kakaoscan.server.domain.user.enums.AuthenticationType;
+import com.kakaoscan.server.domain.user.enums.Role;
+import com.kakaoscan.server.domain.user.repository.UserRepository;
+
+import java.util.ArrayList;
+
+import static com.kakaoscan.server.infrastructure.config.TestConstant.TEST_USER_ID;
+
+public class TestUserDataInitializer {
+    private final UserRepository userRepository;
+
+    public TestUserDataInitializer(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser() {
+        User user = User.builder()
+                .id(0L)
+                .email(TEST_USER_ID)
+                .authenticationType(AuthenticationType.LOCAL)
+                .isEmailVerified(true)
+                .role(Role.USER)
+                .searchHistories(new ArrayList<>())
+                .build();
+
+        user.initializePoint();
+        user.getPoint().addBalance(1000);
+
+        return userRepository.save(user);
+    }
+}
--- server/src/test/java/com/kakaoscan/server/infrastructure/adapter/PointAdapterTest.java
+++ server/src/test/java/com/kakaoscan/server/infrastructure/adapter/PointAdapterTest.java
@@ -1,8 +1,7 @@
 package com.kakaoscan.server.infrastructure.adapter;
 
+import com.kakaoscan.server.application.domain.test.TestUserDataInitializer;
 import com.kakaoscan.server.application.port.PointPort;
-import com.kakaoscan.server.domain.user.enums.AuthenticationType;
-import com.kakaoscan.server.domain.user.enums.Role;
 import com.kakaoscan.server.domain.user.entity.User;
 import com.kakaoscan.server.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.AfterEach;
@@ -13,12 +12,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import java.util.Optional;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import static com.kakaoscan.server.infrastructure.config.TestConstant.TEST_USER_ID;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -30,34 +29,22 @@ class PointAdapterTest {
     @Autowired
     private PointPort pointPort;
 
-    private static final String USER_ID = "test@naver.com";
+    private User user;
+
     private static final int THREAD_COUNT = 10;
 
     @BeforeEach
     @DisplayName("사용자 및 포인트 초기 설정")
     void setUp() {
-        User user = User.builder()
-                .email(USER_ID)
-                .authenticationType(AuthenticationType.LOCAL)
-                .isEmailVerified(true)
-                .role(Role.USER)
-                .build();
-
-        user.initializePoint();
-        user.getPoint().addBalance(1000);
-        userRepository.save(user);
-
+        user = new TestUserDataInitializer(userRepository).createUser();
         System.out.println("----------- Init Balance : " + user.getPoint().getBalance());
     }
 
     @AfterEach
     @DisplayName("최종 포인트 잔액")
     void getPointBalance() {
-        Optional<User> optionalUser = userRepository.findByEmail(USER_ID);
-        if (optionalUser.isPresent()) {
-            int balance = optionalUser.get().getPoint().getBalance();
-            System.out.println("----------- Last Balance : " + balance);
-        }
+        int balance = user.getPoint().getBalance();
+        System.out.println("----------- Last Balance : " + balance);
     }
 
     @Test
@@ -70,7 +57,7 @@ class PointAdapterTest {
         for (int i = 0; i < THREAD_COUNT; i++) {
             executor.submit(() -> {
                 try {
-                    boolean result = pointPort.deductPoints(USER_ID, 1000);
+                    boolean result = pointPort.deductPoints(TEST_USER_ID, 1000);
                     if (result) {
                         successfulDeductions.incrementAndGet();
                     }
