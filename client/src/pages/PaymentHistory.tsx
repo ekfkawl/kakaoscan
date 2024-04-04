43285f0b21cc3bf03840b594df29d840a348869d
ekfkawl <ekfkawl>
2024-04-04 21:39:35 +0900

    chore: always visible depositor text (#22)

--- client/src/pages/PaymentHistory.tsx
+++ client/src/pages/PaymentHistory.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { Badge, Button } from 'flowbite-react';
+import React, {useEffect, useState} from 'react';
+import {Badge, Button} from 'flowbite-react';
 import DateRangePicker from '../components/DateRangePicker';
-import { useFetchData } from '../hooks/useFetchData';
+import {useFetchData} from '../hooks/useFetchData';
 import useDateRangePicker from '../hooks/ui/useDateRangePicker';
-import { formatDate } from '../utils/format/format';
-import { useHttp } from '../hooks/useHttp';
-import { ApiResponse } from '../types/apiResponse';
+import {formatDate} from '../utils/format/format';
+import {useHttp} from '../hooks/useHttp';
+import {ApiResponse} from '../types/apiResponse';
 import MessagePopup from '../components/Popup/MessagePopup';
 import ConfirmPopup from '../components/Popup/ConfirmPopup';
 
@@ -83,55 +83,55 @@ const PaymentHistory = () => {
                                     {formatDate(new Date(product.createdAt))}
                                 </div>
                             </div>
-                            {product.productTransactionStatus === '대기' && (
-                                <div className="pt-5 md:flex md:items-center md:justify-between">
-                                    <div>
-                                        <h4 className="leading-tight tracking-tight text-gray-900 dark:text-white">
-                                            입금해야 할 금액:{' '}
-                                            <span className="text-indigo-600 dark:text-indigo-400">
-                                                {new Intl.NumberFormat('ko-KR').format(product.amount)}원
-                                            </span>
-                                        </h4>
-                                        <h4 className="mt-1 leading-tight tracking-tight text-gray-900 dark:text-white">
-                                            입금자명:{' '}
-                                            <span className="text-indigo-600 dark:text-indigo-400">
-                                                {product.depositor}
-                                            </span>
-                                        </h4>
-                                        <p className="mt-3 text-base font-normal text-gray-800 dark:text-white">
-                                            {transactionsData.data.account}
-                                        </p>
-                                        <p className="mt-3 text-sm font-normal text-gray-500 dark:text-gray-400">
-                                            <strong>* 중요:</strong> 입금 시, 입금자명을{' '}
-                                            <span className="font-semibold">{product.depositor}</span> (으)로 정확히
-                                            기재해 주세요.
-                                        </p>
-                                    </div>
-                                    <div className="flex justify-end items-center">
-                                        <Button
-                                            color="gray"
-                                            onClick={() => {
-                                                setCancelTransactionId(product.id);
-                                                setShowCancelTransactionConfirmPopup(true);
-                                            }}
-                                        >
-                                            <svg
-                                                fill="currentColor"
-                                                viewBox="0 0 20 20"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                className="w-5 h-5 mr-1 -ml-1"
+                            <div className="pt-5 md:flex md:items-center md:justify-between">
+                                {product.productTransactionStatus === '대기' && (
+                                    <>
+                                        <div>
+                                            <h4 className="leading-tight tracking-tight text-gray-900 dark:text-white">
+                                                입금해야 할 금액:{' '}
+                                                <span className="text-indigo-600 dark:text-indigo-400">
+                                                    {new Intl.NumberFormat('ko-KR').format(product.amount)}원
+                                                </span>
+                                            </h4>
+                                            <DepositorInfo depositor={product.depositor} />
+                                            <p className="mt-3 text-base font-normal text-gray-800 dark:text-white">
+                                                {transactionsData.data.account}
+                                            </p>
+                                            <p className="mt-3 text-sm font-normal text-gray-500 dark:text-gray-400">
+                                                <strong>* 중요:</strong> 입금 시, 입금자명을{' '}
+                                                <span className="font-semibold">{product.depositor}</span> (으)로 정확히
+                                                기재해 주세요.
+                                            </p>
+                                        </div>
+                                        <div className="flex justify-end items-center">
+                                            <Button
+                                                color="gray"
+                                                onClick={() => {
+                                                    setCancelTransactionId(product.id);
+                                                    setShowCancelTransactionConfirmPopup(true);
+                                                }}
                                             >
-                                                <path
-                                                    fillRule="evenodd"
-                                                    d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                                                    clipRule="evenodd"
-                                                />
-                                            </svg>
-                                            취소
-                                        </Button>
-                                    </div>
-                                </div>
-                            )}
+                                                <svg
+                                                    fill="currentColor"
+                                                    viewBox="0 0 20 20"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    className="w-5 h-5 mr-1 -ml-1"
+                                                >
+                                                    <path
+                                                        fillRule="evenodd"
+                                                        d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                                                        clipRule="evenodd"
+                                                    />
+                                                </svg>
+                                                취소
+                                            </Button>
+                                        </div>
+                                    </>
+                                )}
+                                {product.productTransactionStatus !== '대기' && (
+                                    <DepositorInfo depositor={product.depositor} />
+                                )}
+                            </div>
                         </div>
                     ))}
                     {showMessage && (
@@ -166,5 +166,11 @@ const PaymentHistory = () => {
         </div>
     );
 };
+
+const DepositorInfo: React.FC<{ depositor: string }> = ({ depositor }) => (
+    <h4 className="mt-1 leading-tight tracking-tight text-gray-900 dark:text-white">
+        입금자명: <span className="text-indigo-600 dark:text-indigo-400">{depositor}</span>
+    </h4>
+);
 
 export default PaymentHistory;
