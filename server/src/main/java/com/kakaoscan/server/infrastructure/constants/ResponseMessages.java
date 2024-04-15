8977039828a32c2b67bf6f6576ab0c56410715ca
ekfkawl <ekfkawl>
2024-04-15 14:47:55 +0900

    chore: modify client display message

--- server/src/main/java/com/kakaoscan/server/infrastructure/constants/ResponseMessages.java
+++ server/src/main/java/com/kakaoscan/server/infrastructure/constants/ResponseMessages.java
@@ -4,7 +4,7 @@ public class ResponseMessages {
     public static final String SEARCH_QUEUE_WAITING = "대기 인원: %d명";
     public static final String SEARCH_WAITING = "서버에 연결 중 입니다..";
     public static final String SEARCH_STARTING = "프로필 조회를 시작합니다..";
-    public static final String SEARCH_ERROR_PING_PONG = "서버 연결에 실패하였습니다.";
+    public static final String SEARCH_ERROR_PING_PONG = "서버 연결에 실패하였습니다. 잠시 후 재시도 해주세요.";
     public static final String SEARCH_INVALID_PHONE_NUMBER = "입력하신 번호를 조회할 수 없습니다.";
     public static final String SEARCH_NOT_PHONE_NUMBER_FORMAT = "올바른 번호 형식이 아닙니다.";
     public static final String SEARCH_CONTINUE = "이전의 작업을 이어서 진행합니다.. %s";
