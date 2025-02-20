3a65c4d58c492cf8fde83044fe1a7421c5a6bdb7
ekfkawl <ekfkawl>
2025-02-20 15:33:45 +0900

    chore: webhook v2 policy

--- server-root/server/src/main/java/com/kakaoscan/server/application/controller/api/WebhookController.java
+++ server-root/server/src/main/java/com/kakaoscan/server/application/controller/api/WebhookController.java
@@ -9,9 +9,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Log4j2
 @RequiredArgsConstructor
@@ -27,5 +31,16 @@ public class WebhookController extends ApiEndpointPrefix {
         productService.approve(Long.valueOf(paymentRequest.getOrderNumber()));
 
         return new ResponseEntity<>(ApiResponse.success(), HttpStatus.OK);
+    }
+
+    @PostMapping("/webhook/v2/payment")
+    public ResponseEntity<Map<String, String>> handlePaymentNotificationV2(@RequestBody WebhookProductPaymentRequest paymentRequest) {
+        log.info("payment notification for order: {}", paymentRequest.getOrderNumber());
+
+        productService.approve(Long.valueOf(paymentRequest.getOrderNumber()));
+
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "success");
+        return ResponseEntity.ok(response);
     }
 }
--- server-root/server/src/main/java/com/kakaoscan/server/domain/product/model/ProductOrderClient.java
+++ server-root/server/src/main/java/com/kakaoscan/server/domain/product/model/ProductOrderClient.java
@@ -1,15 +1,30 @@
 package com.kakaoscan.server.domain.product.model;
 
 import com.kakaoscan.server.application.dto.request.WebhookProductOrderRequest;
+import feign.RequestInterceptor;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "productOrder", url = "${pay.base-url}")
+@FeignClient(name = "productOrder", url = "${pay.base-url}", configuration = ProductOrderClient.FeignConfig.class)
 public interface ProductOrderClient {
     @PostMapping("/order")
     void createProductOrder(@RequestBody WebhookProductOrderRequest order);
 
     @PostMapping("/order-exclude")
     void excludeProductOrder(@RequestBody WebhookProductOrderRequest order);
+
+    @Configuration
+    public static class FeignConfig {
+        @Bean
+        public RequestInterceptor requestInterceptor() {
+            return requestTemplate -> {
+                requestTemplate.header("Content-Type", "application/json");
+                requestTemplate.header("x-api-key", System.getenv("PAY_API_KEY"));
+                requestTemplate.header("x-mall-id", System.getenv("PAY_MALL_ID"));
+            };
+        }
+    }
 }
