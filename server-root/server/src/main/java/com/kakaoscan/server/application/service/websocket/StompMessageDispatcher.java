dbfd720ee7769735226f571fa4ae7bbd871b32a0
ekfkawl <ekfkawl>
2025-09-19 07:48:21 +0900

    chore: 불필요한 조건문 필터 제거

--- server-root/server/src/main/java/com/kakaoscan/server/application/service/websocket/StompMessageDispatcher.java
+++ server-root/server/src/main/java/com/kakaoscan/server/application/service/websocket/StompMessageDispatcher.java
@@ -5,23 +5,19 @@ import com.kakaoscan.server.domain.websocket.model.MessageMetadata;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.messaging.simp.user.SimpUserRegistry;
 import org.springframework.stereotype.Service;
 
 @Log4j2
 @Service
 @RequiredArgsConstructor
 public class StompMessageDispatcher {
-    private final SimpUserRegistry simpUserRegistry;
     private final SimpMessagingTemplate messagingTemplate;
 
     public <T extends MessageMetadata> void sendToUser(T message) {
-        if (simpUserRegistry.getUser(message.getEmail()) != null) {
-            String dest = "/queue/message/search";
-            if (message instanceof PointMessage) {
-                dest = "/queue/message/point";
-            }
-            messagingTemplate.convertAndSendToUser(message.getEmail(), dest, message);
+        String dest = "/queue/message/search";
+        if (message instanceof PointMessage) {
+            dest = "/queue/message/point";
         }
+        messagingTemplate.convertAndSendToUser(message.getEmail(), dest, message);
     }
 }
